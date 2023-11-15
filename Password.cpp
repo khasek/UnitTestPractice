8a2aca7d4d40e87c79a0f28f13d021342b3304d6
khasek <kmhasek@gmail.com>
2023-11-15 12:05:14 -0800

    Adding tests after gcov

--- Password.cpp
+++ Password.cpp
@@ -4,12 +4,16 @@
 using std::string;
 
 /*
-  The function receives a string counts how many times the same character 
-  occurs at the beginning of the string, before any other characters (or the
-  end of the string). The function is case-sensitive so 'Z' is different than
-  'z' and any ASCII characters are allowed.
+The function receives a string counts how many times the same character 
+occurs at the beginning of the string, before any other characters (or the
+end of the string). The function is case-sensitive so 'Z' is different than
+'z' and any ASCII characters are allowed.
 */
 int Password::count_leading_characters(string phrase){
+  if (phrase == "") {
+    return 0;
+  }
+
   int repetition = 1;
   int index = 0;
 
@@ -22,21 +26,69 @@ int Password::count_leading_characters(string phrase){
 
 
 /*
-  returns whether the phrase has both at least one upper-case letter and
-  at least one lower-case letter
-  */
-  bool has_mixed_case(string phrase) {
-    bool has_lowercase = false;
-    bool has_uppercase = false;
-    int index = 0;
+returns whether the phrase has both at least one upper-case letter and
+at least one lower-case letter
+*/
+bool Password::has_mixed_case(string phrase) {
+  bool has_lowercase = false;
+  bool has_uppercase = false;
+  int index = 0;
 
-    while( index < phrase.length()-1 && (!has_lowercase || !has_uppercase)) {
-
-      if (phrase[index] >= 'a' && phrase[index] <= 'a') {
-        has_lowercase = true;
-      } else if (phrase[index] >= 'A' && phrase[index] <= 'Z') {
-        has_uppercase = true;
-      }
-      index++;
-    }
+  if (phrase == "") {
+    return false;
   }
+
+  while( index < phrase.length() && (!has_lowercase || !has_uppercase) ) {
+
+    if (phrase[index] >= 'a' && phrase[index] <= 'z') {
+      has_lowercase = true;
+    } else if (phrase[index] >= 'A' && phrase[index] <= 'Z') {
+      has_uppercase = true;
+    }
+    index++;
+  }
+
+  return (has_lowercase && has_uppercase);
+}
+
+
+/*
+constructor sets the default password to "ChicoCA-95929"
+*/
+Password::Password() {
+  pass_history.push_back("ChicoCA-95929");
+}
+
+
+/*
+receives a password and sets the latest in pass_history to it if and only
+if it meets all criteria:
+  1. The password is at least 8 letters long
+  2. It has no more than 3 of the same leading characters
+  3. It has mixed case (at least one upper case and at least one lower case)
+  4. It was not a previous password in the history
+*/
+void Password::set(string phrase) {
+  if (phrase.length() >= 8 &&
+      count_leading_characters(phrase) <= 3 &&
+      has_mixed_case(phrase) &&
+      phrase != pass_history[pass_history.size()-1]
+  ) {
+    pass_history.push_back(phrase);
+  }
+}
+
+
+/*
+receives a string and authenticates it against the latest password in the
+pass_history, returning true for an exact match or false when it does not match
+or if a password has not been set.
+*/
+bool Password::authenticate(string phrase) {
+  if (pass_history.size() > 0 && 
+      pass_history[pass_history.size()-1] == phrase
+  ) {
+    return true;
+  }
+  return false;
+}
--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -100,20 +100,163 @@ TEST(PasswordTest, includes_newline)
 
 // tests for has_mixed_case() --------------------------------------------------
 
-TEST(PasswordTest, recognize_mixed_case)
+TEST(PasswordTest, check_mixed_case_mixed)
 {
 	Password my_password;
-	int actual = my_password.has_mixed_case("HelloWorld");
+	bool actual = my_password.has_mixed_case("HelloWorld");
 	ASSERT_EQ(true,actual);
 }
 
-/* Test ideas:
-- mixed lower and upper case
-- all lower case
-- all upper case
-- lower case + special characters
-- upper case + special characters
-- no letters, just special characters
-- empty string
-- includes whitespace
-*/
+TEST(PasswordTest, check_mixed_case_all_lower)
+{
+	Password my_password;
+	bool actual = my_password.has_mixed_case("abcxyz");
+	ASSERT_EQ(false,actual);
+}
+
+TEST(PasswordTest, check_mixed_case_all_upper)
+{
+	Password my_password;
+	bool actual = my_password.has_mixed_case("ABCXYZ");
+	ASSERT_EQ(false,actual);
+}
+
+TEST(PasswordTest, check_mixed_case_empty_string)
+{
+	Password my_password;
+	bool actual = my_password.has_mixed_case("");
+	ASSERT_EQ(false,actual);
+}
+
+TEST(PasswordTest, check_mixed_case_all_special)
+{
+	Password my_password;
+	bool actual = my_password.has_mixed_case("1234567890!@#$%^&*()");
+	ASSERT_EQ(false,actual);
+}
+
+TEST(PasswordTest, check_mixed_case_mixed_with_special)
+{
+	Password my_password;
+	bool actual = my_password.has_mixed_case("1234567890A!@#$%^&*()a");
+	ASSERT_EQ(true,actual);
+}
+
+TEST(PasswordTest, check_mixed_case_upper_with_special)
+{
+	Password my_password;
+	bool actual = my_password.has_mixed_case("1234567890AZ!@#$%^&*()");
+	ASSERT_EQ(false,actual);
+}
+
+TEST(PasswordTest, check_mixed_case_lower_with_special)
+{
+	Password my_password;
+	bool actual = my_password.has_mixed_case("1234567890az!@#$%^&*()");
+	ASSERT_EQ(false,actual);
+}
+
+TEST(PasswordTest, check_mixed_case_whitespace)
+{
+	Password my_password;
+	bool actual = my_password.has_mixed_case("\nA \na");
+	ASSERT_EQ(true,actual);
+}
+
+TEST(PasswordTest, check_mixed_case_Aa)
+{
+	Password my_password;
+	bool actual = my_password.has_mixed_case("Aa");
+	ASSERT_EQ(true,actual);
+}
+
+TEST(PasswordTest, check_mixed_case_Zz)
+{
+	Password my_password;
+	bool actual = my_password.has_mixed_case("Zz");
+	ASSERT_EQ(true,actual);
+}
+
+// tests for constructor, set(), authenticate() --------------------------------
+
+TEST(PasswordTest, constructor_sets_expected_default)
+{
+	Password my_password;
+	bool actual = my_password.authenticate("ChicoCA-95929");
+	ASSERT_EQ(true,actual);
+}
+
+TEST(PasswordTest, authenticate_identifies_mismatch)
+{
+	Password my_password;
+	bool actual = my_password.authenticate("Passwords");
+	ASSERT_EQ(false,actual);
+}
+
+TEST(PasswordTest, set_and_auth_password_good)
+{
+	Password my_password;
+	my_password.set("Passwords");
+	bool actual = my_password.authenticate("Passwords");
+	ASSERT_EQ(true,actual);
+}
+
+TEST(PasswordTest, set_password_same_case)
+{
+	Password my_password;
+	my_password.set("passwords");
+	bool actual = my_password.authenticate("passwords");
+	ASSERT_EQ(false,actual);
+}
+
+TEST(PasswordTest, set_password_3_leading_chars)
+{
+	Password my_password;
+	my_password.set("PPPasswords");
+	bool actual = my_password.authenticate("PPPasswords");
+	ASSERT_EQ(true,actual);
+}
+
+TEST(PasswordTest, set_password_too_many_leading_chars)
+{
+	Password my_password;
+	my_password.set("PPPPasswords");
+	bool actual = my_password.authenticate("PPPPasswords");
+	ASSERT_EQ(false,actual);
+}
+
+TEST(PasswordTest, set_password_8_chars)
+{
+	Password my_password;
+	my_password.set("Password");
+	bool actual = my_password.authenticate("Password");
+	ASSERT_EQ(true,actual);
+}
+
+TEST(PasswordTest, set_password_short)
+{
+	Password my_password;
+	my_password.set("Pass");
+	bool actual = my_password.authenticate("Pass");
+	ASSERT_EQ(false,actual);
+}
+
+TEST(PasswordTest, set_password_multiple_times)
+{
+	Password my_password;
+	my_password.set("Passwords1");
+	my_password.set("Passwords2");
+	my_password.set("Passwords3");
+	bool actual = my_password.authenticate("Passwords3");
+	ASSERT_EQ(true,actual);
+}
+
+TEST(PasswordTest, set_password_blank)
+{
+	Password my_password;
+	my_password.set("");
+	bool actual = my_password.authenticate("");
+	ASSERT_EQ(false,actual);
+}
+
+// UNABLE TO TEST FOR REPEAT PASSWORD SUBMISSIONS
